11ab24976981bb8a1f442bed4a05c2e5e20257f5
Hitchhiker-Dave <112595100+Hitchhiker-Dave@users.noreply.github.com>
2023-09-28 00:45:56 -0700

    Fixed File Format
    Reshuffled file format so that git will actually see file changes

---
+++ world.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+#accessing specific child nodes via code
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var polygon2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+
+func _ready():
+	polygon2d.polygon = collision_polygon_2d.polygon
+	print("Hello World")
+	
+func _process(_delta):
+	pass
