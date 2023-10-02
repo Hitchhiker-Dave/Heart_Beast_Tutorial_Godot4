f442c9d95cc23935d7dd3703a7821c4a0fb95eed
Hitchhiker-Dave <112595100+Hitchhiker-Dave@users.noreply.github.com>
2023-10-02 01:33:31 -0700

    Movement Changes 1
    Added Acceleration and Friction to player movement.

--- player.gd
+++ player.gd
@@ -1,8 +1,12 @@
 #template for Character Body 2d
-#Stopped tutorial at 18:19 mark
+#Stopped tutorial at 30:37 mark
 extends CharacterBody2D
 
+#Variables not from the template
+const ACCELERATION = 800
+const FRICTION = 1000
 
+#template variables
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
@@ -17,14 +21,14 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY #move and slide applies delta when changing velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction != 0: #Ie is a direction key bing pressed
+		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 	move_and_slide()
